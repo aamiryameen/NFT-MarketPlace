bbc0cc26c3e1ce339c6406a9c42fed3644cb0584
Aamir Yameen <30290942+aamiryameen@users.noreply.github.com>
2023-07-11 01:26:47 +0500

    connection with db

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const config = require('config');
 
-const db = config.get('mongoURI');
+const db = config.get('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.1');
 
 const connectDB = () => {
     mongoose.connect(db, {
-        
+ 
     }).then(() => console.log(`MongoDB is Connected`))
     .catch(err => {
         console.error(err.message);
